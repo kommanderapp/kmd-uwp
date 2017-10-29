a09dead1d2b17f05d79f47ea8a3ee99d3b22a968
arkoc <aram.koch@gmail.com>
2017-10-29 18:49:30 +0400

    fixed #35

--- kmd.Core/Command/CommandBase.cs
+++ kmd.Core/Command/CommandBase.cs
@@ -7,15 +7,13 @@ namespace kmd.Core.Command
 {
     public abstract class CommandBase : ICommand
     {
+#pragma warning disable CS0067
         public event EventHandler CanExecuteChanged;
+#pragma warning restore CS0067
 
         public bool CanExecute(object parameter) => OnCanExecute(parameter);
 
-        public async void Execute(object parameter)
-        {
-            OnExecute(parameter);
-        }
-
+        public void Execute(object parameter) => OnExecute(parameter);
         protected abstract bool OnCanExecute(object parameter);
 
         protected abstract void OnExecute(object parameter);
---
+++ kmd/Services/DriverPermissionService.cs
@@ -0,0 +1,25 @@
+﻿using kmd.Core.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kmd.Services
+{
+    public static class DriverPermissionService
+    {
+        internal static async Task ShowIfAppropriateAsync()
+        {
+            bool hasShownFirstRun = false;
+            hasShownFirstRun = await Windows.Storage.ApplicationData.Current.LocalSettings.ReadAsync<bool>(nameof(hasShownFirstRun));
+
+            if (!hasShownFirstRun)
+            {
+                await Windows.Storage.ApplicationData.Current.LocalSettings.SaveAsync(nameof(hasShownFirstRun), true);
+                var dialog = new FirstRunDialog();
+                await dialog.ShowAsync();
+            }
+        }
+    }
+}
